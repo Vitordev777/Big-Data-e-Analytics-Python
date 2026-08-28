f79ca78ac47818555516caf37f25ac686d462248
Vitor Felix <vitor.gfelix7@gmail.com>
2026-08-27 21:53:55 -0300

    Create Exercício10.py

---
+++ Aulas/BigData/BigData05_Biblioteca_Numpy/Exercícios/Exercício10.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+# Criação de listas vazias para armazenar dados
+nomes = []#Criando uma variável do tipo Lista vazia para receber os dados de nomes
+idades = []
+alturas = []
+pesos = []
+
+# Abrir e ler o arquivo CSV
+with open(r'C:\Users\vitor\Downloads\dados1.csv', 'r', encoding = 'ISO-8859-1') as batata:
+    lines = batata.readlines()
+
+
+    for line in lines[1:]:  # Ignorar o cabeçalho
+        nome, idade, altura, peso = line.strip().split(',')
+        nomes.append(nome)
+        idades.append(int(idade))
+        alturas.append(int(altura))
+        pesos.append(float(peso))
+
+altura_170 = np.where(np.alturas > 170)
+print(np.nomes[altura_170])
