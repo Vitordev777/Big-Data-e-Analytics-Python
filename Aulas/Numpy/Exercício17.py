740fdbc47c8897f96deaf054fbed134bb12de55e
Vitor Felix <vitor.gfelix7@gmail.com>
2026-08-27 21:55:56 -0300

    Create Exercício17.py

---
+++ Aulas/Numpy/Exercício17.py
@@ -0,0 +1,35 @@
+import numpy as np
+# Criação de listas vazias para armazenar dados
+nomes = []#Criando uma variável do tipo Lista vazia para receber os dados de nomes
+idades = []
+alturas = []
+pesos = []
+
+# Abrir e ler o arquivo CSV
+with open(r'C:\Users\vitor\Downloads\dados1.csv', 'r', encoding = 'ISO-8859-1') as batata:#
+    lines = batata.readlines()
+
+
+    for line in lines[1:]:  # Ignorar o cabeçalho
+        nome, idade, altura, peso = line.strip().split(',')
+        nomes.append(nome)
+        idades.append(int(idade))
+        alturas.append(int(altura))
+        pesos.append(float(peso))
+
+
+nomes_nd = np.array(nomes)
+idades_nd = np.array(idades)
+alturas_nd = np.array(alturas)
+pesos_nd = np.array(pesos)
+
+#Quem é a pessoa mais pesada na base de dados?
+
+maior_peso = np.max(pesos_nd)
+maior_peso
+
+maior_peso_pos = np.argmax(pesos_nd)
+print(maior_peso_pos)
+
+pesssoas_encorpadas = np.where(pesos_nd == maior_peso )
+print(nomes_nd[pesssoas_encorpadas])
