9d6c61b21b66419fece93a00c924162bd1f81dd2
Vitor Felix <vitor.gfelix7@gmail.com>
2026-08-27 21:54:40 -0300

    Create Exemplo09.py

---
+++ Aulas/Pandas/Exemplo09.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+dados={'estado':['SP', 'MG','PR','SP','MG','PR'],
+       'ano':[2019, 2019,2019, 2020,2020,2020],
+       'pop':[45.9, 21.2,16.9,46.6,21.4,17.3]}
+df1=pd.DataFrame(dados)
+
+df2=pd.DataFrame(dados,columns=['ano','estado','pop'])
+
+#Observar Dataframe
+
+#Esta linha mostra a coluna 'estado' do Dataframe df2
+print(df2['estado'])
+
+#Esta linha mostra a coluna 'ano' do Dataframe df2
+print(df2.ano)
+
+#Esta linha mostra o tipo de dados de cada coluna do Dataframe df2
+print(df2.dtypes)
