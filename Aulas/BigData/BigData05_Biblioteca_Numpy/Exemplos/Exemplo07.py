b093697167ff666b976c4525990f924846796298
Vitor Felix <vitor.gfelix7@gmail.com>
2026-08-27 21:53:38 -0300

    Create Exemplo07.py

---
+++ Aulas/BigData/BigData05_Biblioteca_Numpy/Exemplos/Exemplo07.py
@@ -0,0 +1,22 @@
+# Criação de listas vazias para armazenar dados
+nomes = []#Criando uma variável do tipo Lista vazia para receber os dados de nomes
+idades = []
+alturas = []
+pesos = []
+
+# Abrir e ler o arquivo CSV
+with open('dados1.csv', 'r', encoding = 'ISO-8859-1') as batata:#
+    lines = batata.readlines()
+
+
+    for line in lines[1:]:  # Ignorar o cabeçalho
+        nome, idade, altura, peso = line.strip().split(',')
+        nomes.append(nome)
+        idades.append(int(idade))
+        alturas.append(int(altura))
+        pesos.append(float(peso))
+
+print(nomes)
+print(idades)
+print(alturas)
+print(pesos)
