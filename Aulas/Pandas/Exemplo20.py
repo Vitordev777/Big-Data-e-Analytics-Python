2e52d866ef8324c43c81968f6f3d1912f89bfcbf
Vitor Felix <vitor.gfelix7@gmail.com>
2026-08-27 21:54:18 -0300

    Create Exemplo20.py

---
+++ Aulas/Pandas/Exemplo20.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+dados={'estado':['SP', 'MG','PR','SP','MG','PR'],
+       'ano':[2019, 2019,2019, 2020,2020,2020],
+       'pop':[45.9, 21.2,16.9,46.6,21.4,17.3]}
+df1=pd.DataFrame(dados)
+
+df2=pd.DataFrame(dados,columns=['ano','estado','pop'])
+df2['estimativa']=50
+df2['ano']=df2['ano']+2
+
+#Ver dados de linhas
+
+print(df2.iloc[0])
+
+print(df2.iloc[1:3])
+
+print(df2.iloc[1:3,[1,2]])
+
