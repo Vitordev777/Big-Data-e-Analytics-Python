9ad3f8e65e0dcfe19a829b4c5972e06a9a88dd3f
Vitor Felix <vitor.gfelix7@gmail.com>
2026-08-27 21:55:22 -0300

    Create Exercício09.py

---
+++ Aulas/Numpy/Exercício09.py
@@ -0,0 +1,31 @@
+import numpy as np
+# Criação de listas vazias para armazenar dados
+nomes = []#Criando uma variável do tipo Lista vazia para receber os dados de nomes
+idades = []
+alturas = []
+pesos = []
+
+# Abrir e ler o arquivo CSV
+with open(r'C:\Users\vitor\Downloads\dados1.csv', 'r', encoding = 'ISO-8859-1') as batata:#
+    lines = batata.readlines()
+
+
+    for line in lines[1:]:  # Ignorar o cabeçalho
+        nome, idade, altura, peso = line.strip().split(',')
+        nomes.append(nome)
+        idades.append(int(idade))
+        alturas.append(int(altura))
+        pesos.append(float(peso))
+
+
+nomes_nd = np.array(nomes)
+idades_nd = np.array(idades)
+alturas_nd = np.array(alturas)
+pesos_nd = np.array(pesos)
+
+#Quem é a pessoa mais alta na base de dados?
+altura_maxima = np.max(alturas_nd)
+alturas_190 = np.count_nonzero(alturas_nd == altura_maxima)
+print(alturas_190)
+pessoas_190 = nomes_nd[np.where(alturas_nd == altura_maxima)]
+print(pessoas_190)
