8c5c083bff2b0c03dc902c274060cc3bc423171a
Vitor Felix <vitor.gfelix7@gmail.com>
2026-08-27 21:54:20 -0300

    Create Exemplo18.py

---
+++ Aulas/Pandas/Exemplo18.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+dados={'estado':['SP', 'MG','PR','SP','MG','PR'],
+       'ano':[2019, 2019,2019, 2020,2020,2020],
+       'pop':[45.9, 21.2,16.9,46.6,21.4,17.3]}
+df1=pd.DataFrame(dados)
+
+df2=pd.DataFrame(dados,columns=['ano','estado','pop'])
+df2['estimativa']=50
+df2['ano']=df2['ano']+2
+
+#Excluir Linhas
+
+df2.drop([0,1])
+print(df2)
