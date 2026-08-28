a16fa0a89defb4a18f7730839bc3072013981d05
Vitor Felix <vitor.gfelix7@gmail.com>
2026-08-27 21:54:42 -0300

    Create Exemplo08.py

---
+++ Aulas/Pandas/Exemplo08.py
@@ -0,0 +1,12 @@
+#Novo DF a partir o anterior (veja as colunas
+
+import pandas as pd
+
+dados={'estado':['SP', 'MG','PR','SP','MG','PR'],
+       'ano':[2019, 2019,2019, 2020,2020,2020],
+       'pop':[45.9, 21.2,16.9,46.6,21.4,17.3]}
+df1=pd.DataFrame(dados)
+
+df2=pd.DataFrame(dados,columns=['ano','estado','pop'])
+
+print(df2)
