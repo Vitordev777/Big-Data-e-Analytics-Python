69238d36d00c0de6e7bb4d969ec5c8d51892cf8f
Vitor Felix <vitor.gfelix7@gmail.com>
2026-08-27 21:55:40 -0300

    Create Exercício04.py

---
+++ Aulas/Numpy/Exercício04.py
@@ -0,0 +1,34 @@
+import numpy as np
+# Criação de listas vazias para armazenar dados
+nomes = []#Criando uma variável do tipo Lista vazia para receber os dados de nomes
+idades = []
+alturas = []
+pesos = []
+
+# Abrir e ler o arquivo CSV
+with open(r'C:\Users\vitor\Downloads\dados1.csv', 'r', encoding = 'ISO-8859-1') as batata:#
+    lines = batata.readlines()
+
+
+    for line in lines[1:]:  # Ignorar o cabeçalho
+        nome, idade, altura, peso = line.strip().split(',')
+        nomes.append(nome)
+        idades.append(int(idade))
+        alturas.append(int(altura))
+        pesos.append(float(peso))
+
+
+nomes_nd = np.array(nomes)
+idades_nd = np.array(idades)
+alturas_nd = np.array(alturas)
+pesos_nd = np.array(pesos)
+
+#Quantas pessoas possuem peso acima de 70 kg?
+
+pesos_70 = np.where(pesos_nd > 70)
+print(len(nomes_nd[pesos_70]))
+
+#Modo diferente de fazer a mesma coisa
+
+pesos_70 = np.count_nonzero(pesos_nd > 70)
+print(pesos_70)
