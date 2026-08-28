37746868001b6f21e2cc9f902f0617ba6b9cee8f
Vitor Felix <vitor.gfelix7@gmail.com>
2026-08-27 21:52:52 -0300

    Create Exemplo06.py

---
+++ Aulas/BigData/BigData01_Introdução_ao_python/Exemplo06.py
@@ -0,0 +1,6 @@
+c = float(input("Digite a temperatura em graus Fahrenheit: "))
+
+f = (c * 9/5) + 32
+print()
+print(f"Temperatura em Fahrenheit: {f:.1f}f")
+print(f"A temperatura em graus celsius é de: {c:.1f}c")
