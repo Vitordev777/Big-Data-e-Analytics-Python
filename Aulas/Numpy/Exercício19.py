2f81aee28daf61f9fd6df9923ceaa2bdec2cabec
Vitor Felix <vitor.gfelix7@gmail.com>
2026-08-27 21:55:03 -0300

    Create Exercício19.py

---
+++ Aulas/Numpy/Exercício19.py
@@ -0,0 +1,31 @@
+import numpy as np
+# Criação de listas vazias para armazenar dados
+nomes = []#Criando uma variável do tipo Lista vazia para receber os dados de nomes
+idades = []
+alturas = []
+pesos = []
+
+# Abrir e ler o arquivo CSV
+with open(r'C:\Users\vitor\Downloads\dados1.csv', 'r', encoding = 'ISO-8859-1') as batata:#
+    lines = batata.readlines()
+
+
+    for line in lines[1:]:  # Ignorar o cabeçalho
+        nome, idade, altura, peso = line.strip().split(',')
+        nomes.append(nome)
+        idades.append(int(idade))
+        alturas.append(int(altura))
+        pesos.append(float(peso))
+
+
+nomes_nd = np.array(nomes)
+idades_nd = np.array(idades)
+alturas_nd = np.array(alturas)
+pesos_nd = np.array(pesos)
+
+#Qual é a média das idades das pessoas que têm mais de 170 cm de altura?
+
+pessoas_170 = np.where(alturas_nd > 170)
+idades_pessoas_170 = idades_nd[pessoas_170]
+print(round(idades_pessoas_170.mean(),2))
+
