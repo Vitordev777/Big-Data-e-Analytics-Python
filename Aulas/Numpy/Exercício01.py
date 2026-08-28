bd8887b3f5f54fbf79a7585e09965ff4cfdae91a
Vitor Felix <vitor.gfelix7@gmail.com>
2026-08-27 21:55:33 -0300

    Create Exercício01.py

---
+++ Aulas/Numpy/Exercício01.py
@@ -0,0 +1,27 @@
+import numpy as np
+# Criação de listas vazias para armazenar dados
+nomes = []#Criando uma variável do tipo Lista vazia para receber os dados de nomes
+idades = []
+alturas = []
+pesos = []
+
+# Abrir e ler o arquivo CSV
+with open(r'C:\Users\vitor\Downloads\dados1.csv', 'r', encoding = 'ISO-8859-1') as batata:#
+    lines = batata.readlines()
+
+
+    for line in lines[1:]:  # Ignorar o cabeçalho
+        nome, idade, altura, peso = line.strip().split(',')
+        nomes.append(nome)
+        idades.append(int(idade))
+        alturas.append(int(altura))
+        pesos.append(float(peso))
+
+
+
+
+
+nomes_nd = np.array(nomes)
+idades_nd = np.array(idades)
+alturas_nd = np.array(alturas)
+pesos_nd = np.array(pesos)
