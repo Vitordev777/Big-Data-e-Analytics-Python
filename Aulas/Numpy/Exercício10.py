2033e12d3a4259c1186e6a808089ac8f77be7330
Vitor Felix <vitor.gfelix7@gmail.com>
2026-08-27 21:55:19 -0300

    Create Exercício10.py

---
+++ Aulas/Numpy/Exercício10.py
@@ -0,0 +1,30 @@
+import numpy as np
+# Criação de listas vazias para armazenar dados
+nomes = []#Criando uma variável do tipo Lista vazia para receber os dados de nomes
+idades = []
+alturas = []
+pesos = []
+
+# Abrir e ler o arquivo CSV
+with open(r'C:\Users\vitor\Downloads\dados1.csv', 'r', encoding = 'ISO-8859-1') as batata:#
+    lines = batata.readlines()
+
+
+    for line in lines[1:]:  # Ignorar o cabeçalho
+        nome, idade, altura, peso = line.strip().split(',')
+        nomes.append(nome)
+        idades.append(int(idade))
+        alturas.append(int(altura))
+        pesos.append(float(peso))
+
+
+nomes_nd = np.array(nomes)
+idades_nd = np.array(idades)
+alturas_nd = np.array(alturas)
+pesos_nd = np.array(pesos)
+
+# Qual é a altura média das pessoas com idade entre 25 e 30 anos?
+
+idades_25_30 = np.where(np.logical_and(idades_nd >= 25, idades_nd <= 30))
+media_alturas_25_30 = np.mean(alturas_nd[idades_25_30])
+print(round(media_alturas_25_30,2))
